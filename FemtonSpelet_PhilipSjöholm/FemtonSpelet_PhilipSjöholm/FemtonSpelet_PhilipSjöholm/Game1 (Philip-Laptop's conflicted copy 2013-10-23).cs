87a31f37aebe4ac13ded65bd71ff51d23341acba
Philip Sjöholm <philip.sjoholm@gmail.com>
2014-01-02 14:50:50 +0100

    added 15 spelet

---
+++ FemtonSpelet_PhilipSjöholm/FemtonSpelet_PhilipSjöholm/FemtonSpelet_PhilipSjöholm/Game1 (Philip-Laptop's conflicted copy 2013-10-23).cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace FemtonSpelet_PhilipSjöholm
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+
+        // skapa en index som kollar var i arrayen den ska vara 
+        // och sen när du loopar för att kolla checka emot den 
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D _zeldaTexture;
+     
+        Tile[,] tileArray = new Tile[4, 4];
+        Vector2 _borderVector = new Vector2(50, 50);
+        Random _rand = new Random();
+        MouseState _mouseState;
+        MouseState _oldMouseState;
+        int whiteTileIndex_X;
+        int whiteTileIndex_Y;
+        int _numberOfClicks;
+        int index = 0;
+        bool lockGame;
+        Video videoMovie;
+        VideoPlayer _videoPlayer = new VideoPlayer();
+        SpriteFont _spriteFont;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferHeight = 700;
+            graphics.PreferredBackBufferWidth = 700;
+            IsMouseVisible = true;
+
+        }
+
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            // creates new tiles and puts them in the 2dArray
+
+            for (int i = 0; i < tileArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < tileArray.GetLength(1); j++)
+                {
+                    tileArray[i, j] = new Tile(_zeldaTexture, new Vector2(150 * i, 150 * j), new Rectangle(i * 150, j * 150, 150, 150), index, videoMovie, _videoPlayer);
+                    index++;
+                }
+            }
+            
+            
+
+            whiteTileIndex_X = 3;
+            whiteTileIndex_Y = 3;
+
+            for (int i = 0; i < 1; i++)
+            {
+                int pos_X = _rand.Next(0, 4);
+                int pos_Y = _rand.Next(0, 4);
+                // only swaps the white tile to one next to it, making sure that it will only change the board in such a way that it is solveable 
+                if ((pos_X == whiteTileIndex_X - 1) && (whiteTileIndex_Y == pos_Y) || (pos_X == whiteTileIndex_X + 1) && (whiteTileIndex_Y == pos_Y) || (pos_Y == whiteTileIndex_Y - 1) && (whiteTileIndex_X == pos_X) || (pos_Y == whiteTileIndex_Y + 1) && (whiteTileIndex_X == pos_X))
+                {
+                    SwapOut(whiteTileIndex_X, whiteTileIndex_Y, pos_X, pos_Y);
+                }
+            }
+        }
+
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            _zeldaTexture = Content.Load<Texture2D>("zelda");
+            _spriteFont = Content.Load<SpriteFont>("SpriteFont1");
+            videoMovie = Content.Load<Video>("Wildlife");
+        }
+
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            base.Update(gameTime);
+            _videoPlayer.Play(videoMovie);
+            _videoPlayer.IsMuted = true;
+            _oldMouseState = _mouseState;
+            _mouseState = Mouse.GetState();
+            int indexX = (_mouseState.X - 50) / 150;
+            int indexY = (_mouseState.Y - 50) / 150;
+
+            Window.Title = "indexX: " + indexX + "indexY: " + indexY + " " + "Number of Moves: " + _numberOfClicks;
+
+            if (lockGame == false)
+            {
+                if (_mouseState.LeftButton == ButtonState.Pressed && _oldMouseState.LeftButton == ButtonState.Released)
+                {
+                    if (indexY == whiteTileIndex_Y)
+                    {
+                        if (indexX > whiteTileIndex_X)
+                        {
+                            int tempNrOfMoves = indexX - whiteTileIndex_X;
+                            do
+                            {
+                                SwapOut(whiteTileIndex_X, whiteTileIndex_Y, whiteTileIndex_X + 1, whiteTileIndex_Y);
+                                whiteTileIndex_X++;
+                            } while (whiteTileIndex_X <= tempNrOfMoves);
+                        }
+                    }
+                    else
+                    {
+                        int tempNrOfMoves = whiteTileIndex_X - indexX;
+                        do
+                        {
+                            SwapOut(whiteTileIndex_X, whiteTileIndex_Y, whiteTileIndex_X - 1, whiteTileIndex_Y);
+                            whiteTileIndex_X++;
+                        } while (whiteTileIndex_X <= tempNrOfMoves);
+                    }
+                    
+                    /*{
+                    // makes sure that you can only click the tiles next to the white tile
+                   
+                    if (Math.Abs(indexX - whiteTileIndex_X) == 1 && Math.Abs(indexY - whiteTileIndex_Y) == 0 || Math.Abs(indexX - whiteTileIndex_X) == 0 && Math.Abs(indexY - whiteTileIndex_Y) == 1)
+                    {
+                        SwapOut(whiteTileIndex_X, whiteTileIndex_Y, indexX, indexY);
+                        _numberOfClicks++;
+
+                        CheckTiles();
+
+                    }
+                    else if (Math.Abs(indexX - whiteTileIndex_X) == 2 && Math.Abs(indexY - whiteTileIndex_Y) == 0 || Math.Abs(indexX - whiteTileIndex_X) == 0 && Math.Abs(indexY - whiteTileIndex_Y) == 2)
+                    {
+                        for (int i = 0; i < 3; i++)
+                        {
+                            if (indexX >= 0 || indexX <= 3 || indexY >= 0 || indexY <= 3 )
+                            {
+                               SwapOut(whiteTileIndex_X  , whiteTileIndex_Y, indexX, indexY - 1);
+                               SwapOut(whiteTileIndex_X, whiteTileIndex_Y, indexX, indexY);
+                               CheckTiles();  
+                            } 
+                        }
+                       
+                    }
+                    else if (Math.Abs(indexX - whiteTileIndex_X) == 3 && Math.Abs(indexY - whiteTileIndex_Y) == 0 || Math.Abs(indexX - whiteTileIndex_X) == 0 && Math.Abs(indexY - whiteTileIndex_Y) == 3)
+                    {
+                        SwapOut(whiteTileIndex_X, whiteTileIndex_Y, indexX, indexY);
+                    }*/
+                }
+            }
+            // makes sure so that the tiles move when clicked 
+            foreach (Tile t in tileArray)
+            {
+                t.Update();
+            }
+
+
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            base.Draw(gameTime);
+            spriteBatch.Begin();
+            spriteBatch.DrawString(_spriteFont, "TILES CLICKED: ", new Vector2(0, 0), Color.White);
+            spriteBatch.End();
+            for (int i = 0; i < tileArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < tileArray.GetLength(1); j++)
+                {   // ignores drawing the white tile 
+                    if (i == whiteTileIndex_X && j == whiteTileIndex_Y)
+                    {
+                        continue;
+                    }
+
+                    tileArray[i, j].Draw(spriteBatch);
+                }
+            }
+        }
+        private void SwapOut(int posX1, int posY1, int posX2, int posY2)
+        {
+            // swaps out the white tile for the clicked drawn and change the position in 2dArray
+            int tempX;
+            int tempY;
+            tempX = (int)tileArray[posX1, posY1].targetPosX;
+            tempY = (int)tileArray[posX1, posY1].targetPosY;
+
+            tileArray[posX1, posY1].targetPosX = (int)tileArray[posX2, posY2].targetPosX;
+            tileArray[posX1, posY1].targetPosY = (int)tileArray[posX2, posY2].targetPosY;
+
+            tileArray[posX2, posY2].targetPosX = tempX;
+            tileArray[posX2, posY2].targetPosY = tempY;
+
+
+            Tile tempTile;
+            tempTile = tileArray[posX1, posY1];
+            tileArray[posX1, posY1] = tileArray[posX2, posY2];
+            tileArray[posX2, posY2] = tempTile;
+
+            whiteTileIndex_X = posX2;
+            whiteTileIndex_Y = posY2;
+
+
+        }
+        private void CheckTiles()
+        {
+            int loopCycle = 0;
+            int tilesCorrect = 0;
+            // checks tiles for their right position and then if all are correct locks the tiles from moving 
+            for (int i = 0; i < tileArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < tileArray.GetLength(1); j++)
+                {
+                    if (tileArray[i, j].tile_index == loopCycle)
+                    {
+                        Console.WriteLine(loopCycle + "  " + tilesCorrect);
+                        tilesCorrect++;
+                        if (tilesCorrect == 15)
+                        {
+                            Console.WriteLine("YOU WIN !!!");
+                            lockGame = true;
+                        }
+                    }
+                    loopCycle++;
+                }
+            }
+           
+        }
+
+    }
+}
---
+++ FemtonSpelet_PhilipSjöholm/FemtonSpelet_PhilipSjöholm/FemtonSpelet_PhilipSjöholm/Tile.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;  
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+
+namespace FemtonSpelet_PhilipSjöholm
+{
+   
+    class Tile
+    { 
+        Texture2D _tileTexture;
+        Rectangle _rectangle;
+        public Vector2 pos;
+        public int targetPosX;
+        public int targetPosY;
+        public int tile_width;
+        public int tile_height;
+        public int tile_index;
+        
+        int _speed = 2; 
+       
+        Vector2 _offset = new Vector2(50, 50);
+        Vector2 _startpoint;
+        Video _videoMovie;
+        VideoPlayer _videoPlayer;
+
+        public Tile(Texture2D texture1 ,Vector2 firstpos, Rectangle rectangle, int tileIndex, Video vid, VideoPlayer videoPlayer )
+        {
+            tile_index = tileIndex;
+            _rectangle = rectangle;
+            _tileTexture = texture1;
+            _videoPlayer = videoPlayer;
+            pos = firstpos;
+            tile_width = _rectangle.Width;
+            tile_height = _rectangle.Height;
+            _videoMovie = vid;
+           // _rectangle.Width  = _videoMovie.Width / 4;
+           // _rectangle.Height = _videoMovie.Height / 4;
+            _startpoint = new Vector2(-175, -175);
+           
+            targetPosX = (int)pos.X;
+            targetPosY = (int)pos.Y;
+            // makes the tiles start outside border then update makes them move to their correct position 
+            pos = _startpoint;
+            
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+           // Texture2D tempTexture = _videoPlayer.GetTexture();
+            
+            spritebatch.Begin();
+            spritebatch.Draw(_tileTexture,pos + _offset , _rectangle, Color.White);
+            spritebatch.End(); 
+        }
+
+        public void Update()
+        {
+            
+            if (pos.X < targetPosX)
+            {
+                pos.X = pos.X + _speed;
+            }
+            if (pos.X > targetPosX)
+            {
+                pos.X = pos.X - _speed;
+            }
+            if (pos.Y < targetPosY)
+            {
+                pos.Y = pos.Y + _speed;
+            }
+            if (pos.Y > targetPosY)
+            {
+                pos.Y = pos.Y - _speed;
+            }
+        }
+                
+    }
+}
